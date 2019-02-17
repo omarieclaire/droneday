b35331e3c4c1176dcfe05b21bcdad574953fad8d
omarieclaire <marieflanagan@gmail.com>
2019-02-17 17:22:41 -0500

    add photowall

--- photowall.js
+++ photowall.js
@@ -1,16 +1,30 @@
-function getRandomSize(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
-}
+// google drive stuff
 
-var allImages = "";
+// Client ID and API key from the Developer Console
+var CLIENT_ID = 'common-d2ecf';
+var api_key = 'AIzaSyALlQNEoMXQtNqq4v1S61ZiBg98NScwt6I';
 
-for (var i = 0; i < 25; i++) {
-  var width = getRandomSize(200, 400);
-  var height =  getRandomSize(200, 400);
-  //allImages += '<img src="p' + i + '.jpg" />';
-  allImages += '<img src="https://placekitten.com/'+width+'/'+height+'" alt="pretty kitty">';
-}
+var folderId = '1uDSd4nvFzlcstRy2xQ_SRbrO1czS5mGv';
+var url = "https://www.googleapis.com/drive/v3/files?q='" + folderId + "'+in+parents&fields=files(mimeType%2CwebContentLink)&key=" + api_key;
 
-console.log('hi marie');
-document.getElementById('photos').innerHTML = allImages;
-// $('#photos').append(allImages);
+
+const Http = new XMLHttpRequest();
+Http.addEventListener("load", function() {
+  var json = JSON.parse(this.responseText);
+  if(json.error) {
+    console.log("ERROR: " + json.error.errors[0].message);
+  } else {
+    var allImages = '';
+    var files = json.files;
+    for (var i in files) {
+      var file = files[i];
+      if(file.mimeType.startsWith("image")) {
+        allImages += '<img src ="' + file.webContentLink + '" >';
+      }
+    }
+
+    document.getElementById('photos').innerHTML = allImages;
+  }
+});
+Http.open("GET", url);
+Http.send();

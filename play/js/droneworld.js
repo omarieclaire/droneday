fc6438b5a8bd0d32f3f7cb63eb1a0639323216da
omarieclaire <marieflanagan@gmail.com>
2020-05-28 08:55:32 -0400

    update links

--- play/js/droneworld.js
+++ play/js/droneworld.js
@@ -1,148 +1,6 @@
   var earth;
   var coordList = [];
 
-  const eventList = [{
-      name: "Asheville, USA",
-      coord: [35.5951, -82.5515],
-      link: "www"
-    },
-    {
-      name: "Bega, Australia",
-      coord: [-36.6889, 149.8416],
-      link: "www"
-    },
-    {
-      name: "Brandon, Manitoba",
-      coord: [49.8485, -99.9501],
-      link: "www"
-    },
-    {
-      name: "Buenos Aires, Argentina",
-      coord: [-34.6037, -58.3816],
-      link: "www"
-    },
-    {
-      name: "Burlington, USA",
-      coord: [44.4759, -73.2121],
-      link: "www"
-    },
-    {
-      name: "Charlottetown, Canada",
-      coord: [46.2382, -63.1311],
-      link: "www"
-    },
-    {
-      name: "Chicago, USA",
-      coord: [41.8781, -87.6298],
-      link: "www"
-    },
-    {
-      name: "East Lansing, USA",
-      coord: [42.7370, -84.4839],
-      link: "www"
-    },
-    {
-      name: "Edmonton ALTA USA",
-      coord: [53.631611, -113.323975],
-      link: "www"
-    },
-    {
-      name: "Edmonton, Alberta Canada",
-      coord: [53.5461, -113.4938],
-      link: "www"
-    },
-    {
-      name: "Edmonton, Canada",
-      coord: [53.5461, -113.4938],
-      link: "www"
-    },
-    {
-      name: "Guelph, Canada",
-      coord: [43.5448, -80.2482],
-      link: "www"
-    },
-    {
-      name: "Hamilton, Canada",
-      coord: [43.2557, -79.8711],
-      link: "www"
-    },
-    {
-      name: "Kent, UK",
-      coord: [51.2787, 0.5217],
-      link: "www"
-    },
-    {
-      name: "Kingston, Canada",
-      coord: [44.2312, -76.4860],
-      link: "www"
-    },
-    {
-      name: "Lille, France",
-      coord: [50.6292, 3.0573],
-      link: "www"
-    },
-    {
-      name: "Logroño,  Spain",
-      coord: [42.4627, -2.4450],
-      link: "www"
-    },
-    {
-      name: "New Westminster, Canada",
-      coord: [49.2057, -122.9110],
-      link: "www"
-    },
-    {
-      name: "Oaxaca, Mexico",
-      coord: [17.0732, -96.7266],
-      link: "www"
-    },
-    {
-      name: "Olympia, USA",
-      coord: [47.0379, -122.9007],
-      link: "www"
-    },
-    {
-      name: "Ottawa, Ontario",
-      coord: [45.4215, -75.6972],
-      link: "www"
-    },
-    {
-      name: "Peterborough, Canada",
-      coord: [44.3091, -78.3197],
-      link: "www"
-    },
-    {
-      name: "Salt Spring Island, Canada",
-      coord: [48.8167, -123.5089],
-      link: "www"
-    },
-    {
-      name: "Toronto, Canada",
-      coord: [43.6532, -79.3832],
-      link: "www"
-    },
-    {
-      name: "Toronto, Canada",
-      coord: [43.6532, -79.3832],
-      link: "www"
-    },
-    {
-      name: "Vancouver Canada",
-      coord: [49.2827, -123.1207],
-      link: "www"
-    },
-    {
-      name: "Victoria, Canada",
-      coord: [48.4284, -123.3656],
-      link: "www"
-    },
-    {
-      name: "North Carolina, USA",
-      coord: [35.8257, -82.0407],
-      link: "www"
-    }
-  ];
-
   function initialize() {
     var options = {
       atmosphere: true,
@@ -167,8 +25,8 @@
       })
     }
 
-    for (var i = 0; i < eventList.length; i++) {
-      addDestination(eventList[i].coord, eventList[i].name, eventList[i].link);
+    for (var i = 0; i < EVENTLIST.length; i++) {
+      addDestination(EVENTLIST[i].coord, EVENTLIST[i].name, EVENTLIST[i].link);
     }
 
     // var markerCustom = WE.marker([50, -9], '../img/logo-webglearth-white-100.png', 100, 24).addTo(earth);
@@ -214,8 +72,8 @@
     if (ishidden) {
       document.getElementById("listbtn").src = "../img/openeye.png";
       let ul = document.createElement("ul");
-      for (var i = 0; i < eventList.length; i++) {
-        let event = eventList[i];
+      for (var i = 0; i < EVENTLIST.length; i++) {
+        let event = EVENTLIST[i];
 
         let li = document.createElement("li");
         // li.innerHTML = event.name;

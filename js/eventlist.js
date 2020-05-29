bb2cb9b1d2cb98e8bfaba16cb06b156fa553fd83
omarieclaire <marieflanagan@gmail.com>
2020-05-28 21:38:46 -0400

    Add new events

--- js/eventlist.js
+++ js/eventlist.js
@@ -1,141 +1,145 @@
-const EVENTLIST = [{
-    name: "Asheville, USA",
-    coord: [35.5951, -82.5515],
-    link: ""
-  },
-  {
-    name: "Bega, Australia",
-    coord: [-36.6889, 149.8416],
-    link: "https://the5thvolt.com/2020/05/25/world-drone-day-2020/"
-  },
-  {
-    name: "Brandon, Manitoba",
-    coord: [49.8485, -99.9501],
-    link: ""
-  },
-  {
-    name: "Buenos Aires, Argentina",
-    coord: [-34.6037, -58.3816],
-    link: "https://centrodeartesonoro.cultura.gob.ar/info/radio-caso/"
-  },
-  {
-    name: "Burlington, USA",
-    coord: [44.4759, -73.2121],
-    link: "https://www.facebook.com/vermontdroneday"
-  },
-  {
-    name: "Charlottetown, Canada",
-    coord: [46.2382, -63.1311],
-    link: "https://www.facebook.com/events/1143389556019546/"
-  },
-  {
-    name: "Chicago, USA",
-    coord: [41.8781, -87.6298],
-    link: "https://www.twitch.tv/woundsdrone"
-  },
-  {
-    name: "East Lansing, USA",
-    coord: [42.7370, -84.4839],
-    link: "https://youtu.be/NmPd74zECv"
-  },
-  {
-    name: "Edmonton, Canada",
-    coord: [53.631611, -113.323975],
-    link: "https://blog.beams.ca/2020/05/beams-presents-western-canada-drone-day_26.html"
-  },
-  {
-    name: "Edmonton, Canada",
-    coord: [53.5461, -113.4938],
-    link: ""
-  },
-  {
-    name: "Edmonton, Canada",
-    coord: [53.5461, -113.4938],
-    link: ""
-  },
-  {
-    name: "Guelph, Canada",
-    coord: [43.5448, -80.2482],
-    link: ""
-  },
-  {
-    name: "Hamilton, Canada",
-    coord: [43.2557, -79.8711],
-    link: "https://dsfigital.bandcamp.com/"
-  },
-  {
-    name: "Kent, UK",
-    coord: [51.2787, 0.5217],
-    link: "https://youtu.be/uHGgDH1A8ZU"
-  },
-  {
-    name: "Kingston, Canada",
-    coord: [44.2312, -76.4860],
-    link: "http://giss.tv:8000/wildernessradio.mp3"
-  },
-  {
-    name: "Lille, France",
-    coord: [50.6292, 3.0573],
-    link: "https://www.twitch.tv/tommyjarviskngs"
-  },
-  {
-    name: "Logroño,  Spain",
-    coord: [42.4627, -2.4450],
-    link: "http://thurk.org:8103"
-  },
-  {
-    name: "New Westminster, Canada",
-    coord: [49.2057, -122.9110],
-    link: ""
-  },
-  {
-    name: "Oaxaca, Mexico",
-    coord: [17.0732, -96.7266],
-    link: ""
-  },
-  {
-    name: "Olympia, USA",
-    coord: [47.0379, -122.9007],
-    link: "http://instagram.com/bumpobumpu"
-  },
-  {
-    name: "Ottawa, Ontario",
-    coord: [45.4215, -75.6972],
-    link: ""
-  },
-  {
-    name: "Peterborough, Canada",
-    coord: [44.3091, -78.3197],
-    link: ""
-  },
-  {
-    name: "Salt Spring Island, Canada",
-    coord: [48.8167, -123.5089],
-    link: ""
-  },
-  {
-    name: "Toronto, Canada",
-    coord: [43.6532, -79.3832],
-    link: "https://www.facebook.com/events/568133830775878/"
-  },
-  {
-    name: "Toronto, Canada",
-    coord: [43.6532, -79.3832],
-    link: ""
-  },
-  {
-    name: "Vancouver Canada",
-    coord: [49.2827, -123.1207],
-    link: "http://www.kristinfung.com/events/2020/5/30/drone-day-livestream"
-  },
-  {
-    name: "Victoria, Canada",
-    coord: [48.4284, -123.3656],
-    link: "https://youtu.be/cp-yqP9yWUg"
-  },
-  {
-    name: "North Carolina, USA",
-    coord: [35.8257, -82.0407],
-    link: "https://www.youtube.com/watch?v=hdMijUKcNt8"
-  }
+const EVENTLIST = [
+
+  "{
+      name: "Asheville, USA",
+      coord: [35.5951, -82.5515]
+    },{
+      name: "Bega, Australia",
+      coord: [-36.6889, 149.8416]
+    },{
+      name: "Brandon, Manitoba",
+      coord: [49.8485, -99.9501]
+    },{
+      name: "Buenos Aires, Argentina",
+      coord: [-34.6037, -58.3816]
+    },{
+      name: "Burlington, U.S.A.",
+      coord: [44.4759, -73.2121]
+    },{
+      name: "Charlottetown, PEI",
+      coord: [46.2382, -63.1311]
+    },{
+      name: "Chicago, USA",
+      coord: [41.8781, -87.6298]
+    },{
+      name: "East Lansing, USA",
+      coord: [42.7370, -84.4839]
+    },{
+      name: "Edmonton ALTA USA",
+      coord: [53.631611, -113.323975]
+    },{
+      name: "Edmonton, Canada",
+      coord: [53.5461, -113.4938]
+    },{
+      name: "Erie, PA, USA",
+      coord: [42.127949, -80.082581]
+    },{
+      name: "Guelph, Canada",
+      coord: [43.5448, -80.2482]
+    },{
+      name: "Halifax, Canada",
+      coord: [44.6488, -63.5752]
+    },{
+      name: "Hamilton, Canada",
+      coord: [43.2557, -79.8711]
+    },{
+      name: "Kent, U.K.",
+      coord: [51.2787, 0.5217]
+    },{
+      name: "King of Prussia, USA",
+      coord: [40.088322, -75.382133]
+    },{
+      name: "Kingston Ontario Canada",
+      coord: [44.2312, -76.4860]
+    },{
+      name: "Kitchener, Canada",
+      coord: [43.451637, -80.492531]
+    },{
+      name: "Lille, France",
+      coord: [50.6292, 3.0573]
+    },{
+      name: "Logroño,  Spain",
+      coord: [42.4627, -2.4450]
+    },{
+      name: "Miami, USA",
+      coord: [25.761681, -80.191788]
+    },{
+      name: "Montréal, Canada",
+      coord: [45.509060, -73.553360]
+    },{
+      name: "New Westminster, British Columbia, Canada",
+      coord: [49.2057, -122.9110]
+    },{
+      name: "Niagara, Canada",
+      coord: [43.0582, -79.2902]
+    },{
+      name: "Oaxaca, Mexico",
+      coord: [17.0732, -96.7266]
+    },{
+      name: "Olympia WA USA",
+      coord: [47.0379, -122.9007]
+    },{
+      name: "Ottawa, Ontario",
+      coord: [45.4215, -75.6972]
+    },{
+      name: "Paris, France",
+      coord: [48.8566, 2.3522]
+    },{
+      name: "Peterborough, Ontario Canada",
+      coord: [44.3091, -78.3197]
+    },{
+      name: "Queens, NY USA",
+      coord: [40.7282, -73.7949]
+    },{
+      name: "Sackville, Canada",
+      coord: [45.8979, -64.3683]
+    },{
+      name: "Salt Spring Island, British Columbia Canada",
+      coord: [48.8167, -123.5089]
+    },{
+      name: "Samarinda, Indonesia",
+      coord: [-0.5016, 117.1265]
+    },{
+      name: "Someplace",
+      coord: [55.7558, 37.6173]
+    },{
+      name: "Tehran, Iran",
+      coord: [35.6892, 51.3890]
+    },{
+      name: "Toronto, Canada",
+      coord: [43.6532, -79.3832]
+    },{
+      name: "Toronto, Canada",
+      coord: [43.6532, -79.3832]
+    },{
+      name: "Toronto, Canada",
+      coord: [43.6532, -79.3832]
+    },{
+      name: "Toronto, Canada",
+      coord: [43.6532, -79.3832]
+    },{
+      name: "Toronto, Ontario Canada",
+      coord: [43.6532, -79.3832]
+    },{
+      name: "Vancouver BC Canada (the Funktuary)",
+      coord: [49.2827, -123.1207]
+    },{
+      name: "Vancouver, BC Canada",
+      coord: [49.2827, -123.1207]
+    },{
+      name: "Vancouver, BC Canada",
+      coord: [49.2827, -123.1207]
+    },{
+      name: "Vancouver, BC Canada",
+      coord: [49.2827, -123.1207]
+    },{
+      name: "Victoria, Canada",
+      coord: [48.4284, -123.3656]
+    },{
+      name: "Victoria, Canada",
+      coord: [48.4284, 123.3656]
+    },{
+      name: "Western North Carolina, USA",
+      coord: [35.8257, -82.0407]
+    },"
 ];

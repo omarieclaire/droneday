8f65494304b6b53ab276aa297b08b73b5fdb33f5
omarieclaire <marieflanagan@gmail.com>
2020-05-29 12:17:22 -0400

    add loc to events

--- js/eventlist.js
+++ js/eventlist.js
@@ -2,39 +2,39 @@ const EVENTLIST = [
 
   {
     name: "Asheville, USA",
-    coord: [35.5951, -82.5515],
+    coord: [35.589294, -82.554791],
     link: "forrestbaer.com"
   }, {
     name: "Bangkok, Thailand",
-    coord: [],
+    coord: [13.881591, 100.644533],
     link: "https://www.facebook.com/spacewarTH/"
   }, {
     name: "Bega, Australia",
-    coord: [-36.6889, 149.8416],
+    coord: [-36.701317, 149.843248],
     link: "https://the5thvolt.com/2020/05/25/world-drone-day-2020/"
   }, {
     name: "Berlin, Germany",
-    coord: [],
+    coord: [52.52045, 13.40732],
     link: "http://dronerstag.club/"
   }, {
     name: "Berlin, Germany",
-    coord: [],
+    coord: [52.52045, 13.40732],
     link: "seanceonline.de"
   }, {
     name: "Brandon, Manitoba",
-    coord: [49.8485, -99.9501],
+    coord: [49.841243, -99.943882],
     link: ""
   }, {
     name: "Brookfield, IL, USA",
-    coord: [41.8239, -87.8517],
+    coord: [41.824665, -87.850278],
     link: "https://www.youtube.com/channel/UCqPMR4A5ixal9dV01Xj4Ecw/featured"
   }, {
     name: "Buenos Aires, Argentina",
-    coord: [-34.6037, -58.3816],
+    coord: [-34.607357, -58.454595],
     link: "https://centrodeartesonoro.cultura.gob.ar/info/radio-caso/"
   }, {
     name: "Burlington, U.S.A.",
-    coord: [44.4759, -73.2121],
+    coord: [36.083216, -79.452385],
     link: "https://www.facebook.com/vermontdroneday"
   }, {
     name: "Charlottetown, PEI",
@@ -42,163 +42,163 @@ const EVENTLIST = [
     link: "https://www.facebook.com/events/1143389556019546/"
   }, {
     name: "Chicago, USA",
-    coord: [41.8781, -87.6298],
+    coord: [41.87897, -87.66063],
     link: "https://www.twitch.tv/woundsdrone"
   }, {
     name: "Czech Republic",
-    coord: [],
+    coord: [49.88233, 15.377705],
     link: "soon?"
   }, {
     name: "Denmark, Western Australia",
-    coord: [],
+    coord: [-34.898538, 117.118976],
     link: "soon?"
   }, {
     name: "East Lansing, USA",
-    coord: [42.7370, -84.4839],
+    coord: [42.736452, -84.479762],
     link: "https://youtu.be/NmPd74zECv"
   }, {
     name: "Edmonton ALTA USA",
-    coord: [53.631611, -113.323975],
+    coord: [42.672467, -95.305426],
     link: "https://pyramidindigo.bandcamp.com/album/pyramid-indigo-skin-split"
   }, {
     name: "Edmonton, Canada",
-    coord: [53.5461, -113.4938],
+    coord: [53.546218, -113.490371],
     link: ""
   }, {
     name: "Erie, PA, USA",
-    coord: [42.127949, -80.082581],
+    coord: [42.118688, -80.072076],
     link: "https://www.facebook.com/events/684306978803283/"
   }, {
     name: "Guelph, Canada",
-    coord: [43.5448, -80.2482],
+    coord: [43.546728, -80.236799],
     link: ""
   }, {
     name: "Halifax, Canada",
-    coord: [44.6488, -63.5752],
+    coord: [44.64533, -63.57239],
     link: "https://smearframes.bandcamp.com/"
   }, {
     name: "Hamilton, Canada",
-    coord: [43.2557, -79.8711],
+    coord: [43.276797, -80.027986],
     link: "https://dsfigital.bandcamp.com/ "
   }, {
     name: "Johannesburg, South Africa",
-    coord: [],
+    coord: [-26.151045, 27.974334],
     link: "www.further.co.za"
   }, {
     name: "JOHANNESBURG, SOUTH AFRICA",
-    coord: [],
+    coord: [-26.151045, 27.974334],
     link: "Facebook.com/coffinsoup"
   }, {
     name: "Kelaniya, Sri Lanka",
-    coord: [],
+    coord: [6.9553, 79.922],
     link: "https://youtu.be/Nc_Yl1psOcw"
   }, {
     name: "Kent, U.K.",
-    coord: [51.2787, 0.5217],
+    coord: [51.208966, 0.78391],
     link: "https://youtu.be/uHGgDH1A8ZU"
   }, {
     name: "Khon Kaen, Thailand",
-    coord: [],
+    coord: [16.50836, 102.697249],
     link: ""
   }, {
     name: "King of Prussia, USA",
-    coord: [40.088322, -75.382133],
+    coord: [40.099005, -75.381366],
     link: "https://twitch.tv/proem__"
   }, {
     name: "Kingston Ontario Canada",
-    coord: [44.2312, -76.4860],
+    coord: [44.263277, -76.524396],
     link: "https://www.twitch.tv/mrogalsky/"
   }, {
     name: "Kitchener, Canada",
-    coord: [43.451637, -80.492531],
+    coord: [43.418345, -80.468831],
     link: "https://www.instagram.com/ericowenrumble/"
   }, {
     name: "Kolkata,India",
-    coord: [],
+    coord: [22.608067, 88.419453],
     link: ""
   }, {
     name: "Lille, France",
-    coord: [50.6292, 3.0573],
+    coord: [50.632786, 3.059524],
     link: "https://www.twitch.tv/tommyjarviskngs"
   }, {
     name: "Logroño, Spain",
-    coord: [42.4627, -2.4450],
+    coord: [42.464869, -2.451918],
     link: "http://thurk.org:8103"
   }, {
     name: "London",
-    coord: [],
+    coord: [51.509648, -0.099076],
     link: "https://www.twitch.tv/siimphh"
   }, {
     name: "Miami, USA",
-    coord: [25.761681, -80.191788],
+    coord: [25.766368, -80.210268],
     link: "http://locus.creacast.com:9001/mobile_driftwood.ogg (Firefox or Chrome)"
   }, {
     name: "Montreal,",
-    coord: [],
+    coord: [45.572744, -73.586295],
     link: "https://www.twitch.tv/kaseypocius"
   }, {
     name: "Montreal, Canada",
-    coord: [],
+    coord: [45.572744, -73.586295],
     link: "https://www.facebook.com/events/s/jeunesse-cosmique-byod-drone-d/599921094212713/?ti=icl"
   }, {
     name: "Montréal, Canada",
-    coord: [45.509060, -73.553360],
+    coord: [45.572744, -73.586295],
     link: "https://www.twitch.tv/illestpreacha"
   }, {
     name: "Moscow, Russia",
-    coord: [],
+    coord: [55.741469, 37.615561],
     link: ""
   }, {
     name: "New Westminster, British Columbia, Canada",
-    coord: [49.2057, -122.9110],
+    coord: [49.208725, -122.91832],
     link: ""
   }, {
     name: "Niagara, Canada",
-    coord: [43.0582, -79.2902],
+    coord: [43.08261, -79.101562],
     link: "https://www.facebook.com/events/249431069718156"
   }, {
     name: "Nigeria",
-    coord: [],
+    coord: [9.404974, 7.607418],
     link: ""
   }, {
     name: "Oaxaca, Mexico",
-    coord: [17.0732, -96.7266],
+    coord: [17.025593, -96.819523],
     link: ""
   }, {
     name: "Olympia WA USA",
-    coord: [47.0379, -122.9007],
+    coord: [47.047456, -122.896902],
     link: "@bumpobumpu"
   }, {
     name: "Ottawa, Ontario",
-    coord: [45.4215, -75.6972],
+    coord: [45.209415, -75.783876],
     link: ""
   }, {
     name: "Paris, France",
-    coord: [48.8566, 2.3522],
+    coord: [48.859116, 2.331839],
     link: "https://linktr.ee/a_f_alfla"
   }, {
     name: "Peterborough, Ontario Canada",
-    coord: [44.3091, -78.3197],
+    coord: [44.597936, -78.103469],
     link: ""
   }, {
     name: "Portland, OR USA",
-    coord: [],
+    coord: [45.533467, -122.650095],
     link: "https://verns.world"
   }, {
     name: "Queens, NY USA",
-    coord: [40.7282, -73.7949],
+    coord: [40.718804, -73.805154],
     link: "jensuis.bandcamp.com"
   }, {
     name: "Regina",
-    coord: [],
+    coord: [50.448205, -104.619116],
     link: "https://www.facebook.com/events/295125491505458/"
   }, {
     name: "Richmond, VA, US",
-    coord: [],
+    coord: [37.519231, -77.473927],
     link: "https://tavishi.bandcamp.com/"
   }, {
     name: "Sackville, Canada",
-    coord: [45.8979, -64.3683],
+    coord: [45.916049, -64.390572],
     link: "https://m.facebook.com/blackdimes"
   }, {
     name: "Salt Spring Island, Canada",
@@ -206,75 +206,75 @@ const EVENTLIST = [
     link: "https://www.facebook.com/events/549807732388271/ "
   }, {
     name: "Samarinda, Indonesia",
-    coord: [-0.5016, 117.1265],
+    coord: [-0.490842, 117.155943],
     link: "Play.visualjalanan.org"
   }, {
     name: "Someplace",
-    coord: [55.7558, 37.6173],
+    coord: [41.9147, -71.90894],
     link: "https://www.facebook.com/fracturespace/"
   }, {
     name: "Tehran, Iran",
-    coord: [35.6892, 51.3890],
+    coord: [35.69439, 51.42151],
     link: "www.xerxesthedark.bandcamp.com"
   }, {
     name: "Thailand",
-    coord: [],
+    coord: [15.512883, 101.301707],
     link: "https://www.facebook.com/parasabda"
   }, {
     name: "Toronto, Canada",
-    coord: [43.6532, -79.3832],
+    coord: [43.64877, -79.38171],
     link: "https://www.facebook.com/events/568133830775878/"
   }, {
     name: "Toronto, Canada",
-    coord: [43.6532, -79.3832],
+    coord: [43.64877, -79.38171],
     link: "Streaming on Instagram Live: @jsg_amok"
   }, {
     name: "Toronto, Canada",
-    coord: [43.6532, -79.3832],
+    coord: [43.64877, -79.38171],
     link: "https://www.youtube.com/watch?v=15B-AvR5cPo&t=4s"
   }, {
     name: "Toronto, Canada",
-    coord: [43.6532, -79.3832],
+    coord: [43.64877, -79.38171],
     link: "sunrise drone: https://youtu.be/kX58cXssspw and then a sunset drone: https://youtu.be/xfT-3sQ3m8A"
   }, {
     name: "Toronto, Canada",
-    coord: [],
+    coord: [43.64877, -79.38171],
     link: "https://www.facebook.com/events/339000870350969/"
   }, {
     name: "Vancouver BC Canada (the Funktuary)",
-    coord: [49.2827, -123.1207],
+    coord: [49.287469, -123.119579],
     link: "http://www.kristinfung.com/events/2020/5/30/drone-day-livestream"
   }, {
     name: "Vancouver, BC Canada",
-    coord: [49.2827, -123.1207],
+    coord: [49.2819, -123.11874],
     link: "graham-peterson.bandcamp.com"
   }, {
     name: "Vancouver, BC Canada",
-    coord: [49.2827, -123.1207],
+    coord: [49.2819, -123.11874],
     link: "Groovedwhaleproject.com"
   }, {
     name: "Vancouver, BC Canada",
-    coord: [49.2827, -123.1207],
+    coord: [49.2819, -123.11874],
     link: "Twitch.com/x41_"
   }, {
     name: "Victoria, Canada",
-    coord: [48.4284, -123.3656],
+    coord: [48.428362, -123.358788],
     link: "https://youtu.be/cp-yqP9yWUg"
   }, {
     name: "Victoria, Canada",
-    coord: [48.4284, 123.3656],
+    coord: [48.428362, -123.358788],
     link: "https://youtu.be/cp-yqP9yWUg"
   }, {
     name: "Welland, Canada",
-    coord: [],
+    coord: [42.992832, -79.228938],
     link: "coming soon"
   }, {
     name: "Western North Carolina, USA",
-    coord: [35.8257, -82.0407],
+    coord: [35.387362, -78.455056],
     link: "https://www.youtube.com/watch?v=hdMijUKcNt8"
   }, {
     name: "Yogyakarta, Indonesia",
-    coord: [],
+    coord: [-7.786164, 110.399303],
     link: "deakarina.net"
   },
 ];

5605170e7f5c3c61ebb6e7111c32eac1a89dc4b1
omarieclaire <marieflanagan@gmail.com>
2020-05-29 11:41:41 -0400

    add links to events

--- js/eventlist.js
+++ js/eventlist.js
@@ -3,15 +3,31 @@ const EVENTLIST = [
   {
     name: "Asheville, USA",
     coord: [35.5951, -82.5515],
-    link: "http://forrestbaer.com"
+    link: "forrestbaer.com"
+  }, {
+    name: "Bangkok, Thailand",
+    coord: [],
+    link: "https://www.facebook.com/spacewarTH/"
   }, {
     name: "Bega, Australia",
     coord: [-36.6889, 149.8416],
     link: "https://the5thvolt.com/2020/05/25/world-drone-day-2020/"
   }, {
+    name: "Berlin, Germany",
+    coord: [],
+    link: "http://dronerstag.club/"
+  }, {
+    name: "Berlin, Germany",
+    coord: [],
+    link: "seanceonline.de"
+  }, {
     name: "Brandon, Manitoba",
     coord: [49.8485, -99.9501],
     link: ""
+  }, {
+    name: "Brookfield, IL, USA",
+    coord: [41.8239, -87.8517],
+    link: "https://www.youtube.com/channel/UCqPMR4A5ixal9dV01Xj4Ecw/featured"
   }, {
     name: "Buenos Aires, Argentina",
     coord: [-34.6037, -58.3816],
@@ -28,6 +44,14 @@ const EVENTLIST = [
     name: "Chicago, USA",
     coord: [41.8781, -87.6298],
     link: "https://www.twitch.tv/woundsdrone"
+  }, {
+    name: "Czech Republic",
+    coord: [],
+    link: "soon?"
+  }, {
+    name: "Denmark, Western Australia",
+    coord: [],
+    link: "soon?"
   }, {
     name: "East Lansing, USA",
     coord: [42.7370, -84.4839],
@@ -57,9 +81,25 @@ const EVENTLIST = [
     coord: [43.2557, -79.8711],
     link: "https://dsfigital.bandcamp.com/ "
   }, {
+    name: "Johannesburg, South Africa",
+    coord: [],
+    link: "www.further.co.za"
+  }, {
+    name: "JOHANNESBURG, SOUTH AFRICA",
+    coord: [],
+    link: "Facebook.com/coffinsoup"
+  }, {
+    name: "Kelaniya, Sri Lanka",
+    coord: [],
+    link: "https://youtu.be/Nc_Yl1psOcw"
+  }, {
     name: "Kent, U.K.",
     coord: [51.2787, 0.5217],
     link: "https://youtu.be/uHGgDH1A8ZU"
+  }, {
+    name: "Khon Kaen, Thailand",
+    coord: [],
+    link: ""
   }, {
     name: "King of Prussia, USA",
     coord: [40.088322, -75.382133],
@@ -73,6 +113,10 @@ const EVENTLIST = [
     coord: [43.451637, -80.492531],
     link: "https://www.instagram.com/ericowenrumble/"
   }, {
+    name: "Kolkata,India",
+    coord: [],
+    link: ""
+  }, {
     name: "Lille, France",
     coord: [50.6292, 3.0573],
     link: "https://www.twitch.tv/tommyjarviskngs"
@@ -81,13 +125,29 @@ const EVENTLIST = [
     coord: [42.4627, -2.4450],
     link: "http://thurk.org:8103"
   }, {
+    name: "London",
+    coord: [],
+    link: "https://www.twitch.tv/siimphh"
+  }, {
     name: "Miami, USA",
     coord: [25.761681, -80.191788],
     link: "http://locus.creacast.com:9001/mobile_driftwood.ogg (Firefox or Chrome)"
   }, {
+    name: "Montreal,",
+    coord: [],
+    link: "https://www.twitch.tv/kaseypocius"
+  }, {
+    name: "Montreal, Canada",
+    coord: [],
+    link: "https://www.facebook.com/events/s/jeunesse-cosmique-byod-drone-d/599921094212713/?ti=icl"
+  }, {
     name: "Montréal, Canada",
     coord: [45.509060, -73.553360],
     link: "https://www.twitch.tv/illestpreacha"
+  }, {
+    name: "Moscow, Russia",
+    coord: [],
+    link: ""
   }, {
     name: "New Westminster, British Columbia, Canada",
     coord: [49.2057, -122.9110],
@@ -96,6 +156,10 @@ const EVENTLIST = [
     name: "Niagara, Canada",
     coord: [43.0582, -79.2902],
     link: "https://www.facebook.com/events/249431069718156"
+  }, {
+    name: "Nigeria",
+    coord: [],
+    link: ""
   }, {
     name: "Oaxaca, Mexico",
     coord: [17.0732, -96.7266],
@@ -117,17 +181,29 @@ const EVENTLIST = [
     coord: [44.3091, -78.3197],
     link: ""
   }, {
+    name: "Portland, OR USA",
+    coord: [],
+    link: "https://verns.world"
+  }, {
     name: "Queens, NY USA",
     coord: [40.7282, -73.7949],
-    link: "https://jensuis.bandcamp.com"
+    link: "jensuis.bandcamp.com"
+  }, {
+    name: "Regina",
+    coord: [],
+    link: "https://www.facebook.com/events/295125491505458/"
+  }, {
+    name: "Richmond, VA, US",
+    coord: [],
+    link: "https://tavishi.bandcamp.com/"
   }, {
     name: "Sackville, Canada",
     coord: [45.8979, -64.3683],
     link: "https://m.facebook.com/blackdimes"
   }, {
-    name: "Salt Spring Island, British Columbia Canada",
+    name: "Salt Spring Island, Canada",
     coord: [48.8167, -123.5089],
-    link: ""
+    link: "https://www.facebook.com/events/549807732388271/ "
   }, {
     name: "Samarinda, Indonesia",
     coord: [-0.5016, 117.1265],
@@ -140,6 +216,10 @@ const EVENTLIST = [
     name: "Tehran, Iran",
     coord: [35.6892, 51.3890],
     link: "www.xerxesthedark.bandcamp.com"
+  }, {
+    name: "Thailand",
+    coord: [],
+    link: "https://www.facebook.com/parasabda"
   }, {
     name: "Toronto, Canada",
     coord: [43.6532, -79.3832],
@@ -157,9 +237,9 @@ const EVENTLIST = [
     coord: [43.6532, -79.3832],
     link: "sunrise drone: https://youtu.be/kX58cXssspw and then a sunset drone: https://youtu.be/xfT-3sQ3m8A"
   }, {
-    name: "Toronto, Ontario Canada",
-    coord: [43.6532, -79.3832],
-    link: ""
+    name: "Toronto, Canada",
+    coord: [],
+    link: "https://www.facebook.com/events/339000870350969/"
   }, {
     name: "Vancouver BC Canada (the Funktuary)",
     coord: [49.2827, -123.1207],
@@ -185,8 +265,16 @@ const EVENTLIST = [
     coord: [48.4284, 123.3656],
     link: "https://youtu.be/cp-yqP9yWUg"
   }, {
+    name: "Welland, Canada",
+    coord: [],
+    link: "coming soon"
+  }, {
     name: "Western North Carolina, USA",
     coord: [35.8257, -82.0407],
     link: "https://www.youtube.com/watch?v=hdMijUKcNt8"
-  }
+  }, {
+    name: "Yogyakarta, Indonesia",
+    coord: [],
+    link: "deakarina.net"
+  },
 ];

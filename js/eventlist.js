fc94a861a65a19cb5248723d5cdc9dc31ea8cda1
omarieclaire <marieflanagan@gmail.com>
2020-05-28 22:46:12 -0400

    add links to events

--- js/eventlist.js
+++ js/eventlist.js
@@ -1,145 +1,192 @@
 const EVENTLIST = [
 
- {
-      name: "Asheville, USA",
-      coord: [35.5951, -82.5515]
-    },{
-      name: "Bega, Australia",
-      coord: [-36.6889, 149.8416]
-    },{
-      name: "Brandon, Manitoba",
-      coord: [49.8485, -99.9501]
-    },{
-      name: "Buenos Aires, Argentina",
-      coord: [-34.6037, -58.3816]
-    },{
-      name: "Burlington, U.S.A.",
-      coord: [44.4759, -73.2121]
-    },{
-      name: "Charlottetown, PEI",
-      coord: [46.2382, -63.1311]
-    },{
-      name: "Chicago, USA",
-      coord: [41.8781, -87.6298]
-    },{
-      name: "East Lansing, USA",
-      coord: [42.7370, -84.4839]
-    },{
-      name: "Edmonton ALTA USA",
-      coord: [53.631611, -113.323975]
-    },{
-      name: "Edmonton, Canada",
-      coord: [53.5461, -113.4938]
-    },{
-      name: "Erie, PA, USA",
-      coord: [42.127949, -80.082581]
-    },{
-      name: "Guelph, Canada",
-      coord: [43.5448, -80.2482]
-    },{
-      name: "Halifax, Canada",
-      coord: [44.6488, -63.5752]
-    },{
-      name: "Hamilton, Canada",
-      coord: [43.2557, -79.8711]
-    },{
-      name: "Kent, U.K.",
-      coord: [51.2787, 0.5217]
-    },{
-      name: "King of Prussia, USA",
-      coord: [40.088322, -75.382133]
-    },{
-      name: "Kingston Ontario Canada",
-      coord: [44.2312, -76.4860]
-    },{
-      name: "Kitchener, Canada",
-      coord: [43.451637, -80.492531]
-    },{
-      name: "Lille, France",
-      coord: [50.6292, 3.0573]
-    },{
-      name: "Logroño,  Spain",
-      coord: [42.4627, -2.4450]
-    },{
-      name: "Miami, USA",
-      coord: [25.761681, -80.191788]
-    },{
-      name: "Montréal, Canada",
-      coord: [45.509060, -73.553360]
-    },{
-      name: "New Westminster, British Columbia, Canada",
-      coord: [49.2057, -122.9110]
-    },{
-      name: "Niagara, Canada",
-      coord: [43.0582, -79.2902]
-    },{
-      name: "Oaxaca, Mexico",
-      coord: [17.0732, -96.7266]
-    },{
-      name: "Olympia WA USA",
-      coord: [47.0379, -122.9007]
-    },{
-      name: "Ottawa, Ontario",
-      coord: [45.4215, -75.6972]
-    },{
-      name: "Paris, France",
-      coord: [48.8566, 2.3522]
-    },{
-      name: "Peterborough, Ontario Canada",
-      coord: [44.3091, -78.3197]
-    },{
-      name: "Queens, NY USA",
-      coord: [40.7282, -73.7949]
-    },{
-      name: "Sackville, Canada",
-      coord: [45.8979, -64.3683]
-    },{
-      name: "Salt Spring Island, British Columbia Canada",
-      coord: [48.8167, -123.5089]
-    },{
-      name: "Samarinda, Indonesia",
-      coord: [-0.5016, 117.1265]
-    },{
-      name: "Someplace",
-      coord: [55.7558, 37.6173]
-    },{
-      name: "Tehran, Iran",
-      coord: [35.6892, 51.3890]
-    },{
-      name: "Toronto, Canada",
-      coord: [43.6532, -79.3832]
-    },{
-      name: "Toronto, Canada",
-      coord: [43.6532, -79.3832]
-    },{
-      name: "Toronto, Canada",
-      coord: [43.6532, -79.3832]
-    },{
-      name: "Toronto, Canada",
-      coord: [43.6532, -79.3832]
-    },{
-      name: "Toronto, Ontario Canada",
-      coord: [43.6532, -79.3832]
-    },{
-      name: "Vancouver BC Canada (the Funktuary)",
-      coord: [49.2827, -123.1207]
-    },{
-      name: "Vancouver, BC Canada",
-      coord: [49.2827, -123.1207]
-    },{
-      name: "Vancouver, BC Canada",
-      coord: [49.2827, -123.1207]
-    },{
-      name: "Vancouver, BC Canada",
-      coord: [49.2827, -123.1207]
-    },{
-      name: "Victoria, Canada",
-      coord: [48.4284, -123.3656]
-    },{
-      name: "Victoria, Canada",
-      coord: [48.4284, 123.3656]
-    },{
-      name: "Western North Carolina, USA",
-      coord: [35.8257, -82.0407]
-    },
+  {
+    name: "Asheville, USA",
+    coord: [35.5951, -82.5515],
+    link: "http://forrestbaer.com"
+  }, {
+    name: "Bega, Australia",
+    coord: [-36.6889, 149.8416],
+    link: "https://the5thvolt.com/2020/05/25/world-drone-day-2020/"
+  }, {
+    name: "Brandon, Manitoba",
+    coord: [49.8485, -99.9501],
+    link: ""
+  }, {
+    name: "Buenos Aires, Argentina",
+    coord: [-34.6037, -58.3816],
+    link: "https://centrodeartesonoro.cultura.gob.ar/info/radio-caso/"
+  }, {
+    name: "Burlington, U.S.A.",
+    coord: [44.4759, -73.2121],
+    link: "https://www.facebook.com/vermontdroneday"
+  }, {
+    name: "Charlottetown, PEI",
+    coord: [46.2382, -63.1311],
+    link: "https://www.facebook.com/events/1143389556019546/"
+  }, {
+    name: "Chicago, USA",
+    coord: [41.8781, -87.6298],
+    link: "https://www.twitch.tv/woundsdrone"
+  }, {
+    name: "East Lansing, USA",
+    coord: [42.7370, -84.4839],
+    link: "https://youtu.be/NmPd74zECv"
+  }, {
+    name: "Edmonton ALTA USA",
+    coord: [53.631611, -113.323975],
+    link: "https://pyramidindigo.bandcamp.com/album/pyramid-indigo-skin-split"
+  }, {
+    name: "Edmonton, Canada",
+    coord: [53.5461, -113.4938],
+    link: ""
+  }, {
+    name: "Erie, PA, USA",
+    coord: [42.127949, -80.082581],
+    link: "https://www.facebook.com/events/684306978803283/"
+  }, {
+    name: "Guelph, Canada",
+    coord: [43.5448, -80.2482],
+    link: ""
+  }, {
+    name: "Halifax, Canada",
+    coord: [44.6488, -63.5752],
+    link: "https://smearframes.bandcamp.com/"
+  }, {
+    name: "Hamilton, Canada",
+    coord: [43.2557, -79.8711],
+    link: "https://dsfigital.bandcamp.com/ "
+  }, {
+    name: "Kent, U.K.",
+    coord: [51.2787, 0.5217],
+    link: "https://youtu.be/uHGgDH1A8ZU"
+  }, {
+    name: "King of Prussia, USA",
+    coord: [40.088322, -75.382133],
+    link: "https://twitch.tv/proem__"
+  }, {
+    name: "Kingston Ontario Canada",
+    coord: [44.2312, -76.4860],
+    link: "https://www.twitch.tv/mrogalsky/"
+  }, {
+    name: "Kitchener, Canada",
+    coord: [43.451637, -80.492531],
+    link: "https://www.instagram.com/ericowenrumble/"
+  }, {
+    name: "Lille, France",
+    coord: [50.6292, 3.0573],
+    link: "https://www.twitch.tv/tommyjarviskngs"
+  }, {
+    name: "Logroño, Spain",
+    coord: [42.4627, -2.4450],
+    link: "http://thurk.org:8103"
+  }, {
+    name: "Miami, USA",
+    coord: [25.761681, -80.191788],
+    link: "http://locus.creacast.com:9001/mobile_driftwood.ogg (Firefox or Chrome)"
+  }, {
+    name: "Montréal, Canada",
+    coord: [45.509060, -73.553360],
+    link: "https://www.twitch.tv/illestpreacha"
+  }, {
+    name: "New Westminster, British Columbia, Canada",
+    coord: [49.2057, -122.9110],
+    link: ""
+  }, {
+    name: "Niagara, Canada",
+    coord: [43.0582, -79.2902],
+    link: "https://www.facebook.com/events/249431069718156"
+  }, {
+    name: "Oaxaca, Mexico",
+    coord: [17.0732, -96.7266],
+    link: ""
+  }, {
+    name: "Olympia WA USA",
+    coord: [47.0379, -122.9007],
+    link: "@bumpobumpu"
+  }, {
+    name: "Ottawa, Ontario",
+    coord: [45.4215, -75.6972],
+    link: ""
+  }, {
+    name: "Paris, France",
+    coord: [48.8566, 2.3522],
+    link: "https://linktr.ee/a_f_alfla"
+  }, {
+    name: "Peterborough, Ontario Canada",
+    coord: [44.3091, -78.3197],
+    link: ""
+  }, {
+    name: "Queens, NY USA",
+    coord: [40.7282, -73.7949],
+    link: "https://jensuis.bandcamp.com"
+  }, {
+    name: "Sackville, Canada",
+    coord: [45.8979, -64.3683],
+    link: "https://m.facebook.com/blackdimes"
+  }, {
+    name: "Salt Spring Island, British Columbia Canada",
+    coord: [48.8167, -123.5089],
+    link: ""
+  }, {
+    name: "Samarinda, Indonesia",
+    coord: [-0.5016, 117.1265],
+    link: "Play.visualjalanan.org"
+  }, {
+    name: "Someplace",
+    coord: [55.7558, 37.6173],
+    link: "https://www.facebook.com/fracturespace/"
+  }, {
+    name: "Tehran, Iran",
+    coord: [35.6892, 51.3890],
+    link: "www.xerxesthedark.bandcamp.com"
+  }, {
+    name: "Toronto, Canada",
+    coord: [43.6532, -79.3832],
+    link: "https://www.facebook.com/events/568133830775878/"
+  }, {
+    name: "Toronto, Canada",
+    coord: [43.6532, -79.3832],
+    link: "Streaming on Instagram Live: @jsg_amok"
+  }, {
+    name: "Toronto, Canada",
+    coord: [43.6532, -79.3832],
+    link: "https://www.youtube.com/watch?v=15B-AvR5cPo&t=4s"
+  }, {
+    name: "Toronto, Canada",
+    coord: [43.6532, -79.3832],
+    link: "sunrise drone: https://youtu.be/kX58cXssspw and then a sunset drone: https://youtu.be/xfT-3sQ3m8A"
+  }, {
+    name: "Toronto, Ontario Canada",
+    coord: [43.6532, -79.3832],
+    link: ""
+  }, {
+    name: "Vancouver BC Canada (the Funktuary)",
+    coord: [49.2827, -123.1207],
+    link: "http://www.kristinfung.com/events/2020/5/30/drone-day-livestream"
+  }, {
+    name: "Vancouver, BC Canada",
+    coord: [49.2827, -123.1207],
+    link: "graham-peterson.bandcamp.com"
+  }, {
+    name: "Vancouver, BC Canada",
+    coord: [49.2827, -123.1207],
+    link: "Groovedwhaleproject.com"
+  }, {
+    name: "Vancouver, BC Canada",
+    coord: [49.2827, -123.1207],
+    link: "Twitch.com/x41_"
+  }, {
+    name: "Victoria, Canada",
+    coord: [48.4284, -123.3656],
+    link: "https://youtu.be/cp-yqP9yWUg"
+  }, {
+    name: "Victoria, Canada",
+    coord: [48.4284, 123.3656],
+    link: "https://youtu.be/cp-yqP9yWUg"
+  }, {
+    name: "Western North Carolina, USA",
+    coord: [35.8257, -82.0407],
+    link: "https://www.youtube.com/watch?v=hdMijUKcNt8"
+  }
 ];

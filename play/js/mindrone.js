f271049fb37252e6fa2132caec9daa123291cb1a
Marie Claire LeBlanc Flanagan <marieflanagan@gmail.com>
2021-06-07 16:13:58 -0400

    update dates and add links

---
+++ play/js/mindrone.js
@@ -0,0 +1,24 @@
+// window.addEventListener("load", () => {
+
+
+  window.onload = function(){
+
+    var myAudio = document.getElementById('my-audio');
+    var play = document.getElementById('play');
+    var pause = document.getElementById('pause');
+
+    // associate functions with the 'onclick' events
+    play.onclick = playAudio;
+    pause.onclick = pauseAudio;
+
+    function playAudio() {
+      myAudio.play();
+    }
+
+    function pauseAudio() {
+      myAudio.pause();
+    }
+
+  }
+
+// 192.168.1.52:8000

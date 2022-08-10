50cb00ee0f01441a65911ddedc4481568d7aaaf9
Marie Claire LeBlanc Flanagan <marieflanagan@gmail.com>
2022-08-10 16:26:18 -0400

    readd photowall

---
+++ photowall.js
@@ -0,0 +1,22 @@
+// Client ID and API key from the Developer Console
+var CLIENT_ID = 'common-d2ecf';
+var api_key = 'AIzaSyALlQNEoMXQtNqq4v1S61ZiBg98NScwt6I';
+
+var folderId = '1uDSd4nvFzlcstRy2xQ_SRbrO1czS5mGv';
+var url = "https://www.googleapis.com/drive/v3/files?q='" + folderId + "'+in+parents&fields=files(mimeType%2CwebContentLink)&key=" + api_key;
+
+const Http = new XMLHttpRequest();
+Http.addEventListener("load", function() {
+  var json = JSON.parse(this.responseText);
+  if(json.error) {
+    console.log("ERROR: " + json.error.errors[0].message);
+  } else {
+    var allImages = '';
+    var files = json.files;
+    for (var i in files) {
+      document.getElementById('photos').innerHTML = allImages;
+    }
+  }
+});
+Http.open("GET", url);
+Http.send();
